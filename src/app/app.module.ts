7224d11eaa3675a0c414a040817c4b2ede4e694a
AnthonyYu75 <74040597+AnthonyYu75@users.noreply.github.com>
2023-09-12 15:49:05 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewComponent } from './new/new.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'new', component: NewComponent },
+      { path: 'home', component: HomeComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    JumbotronComponent,
+    CategoriesComponent,
+    FooterComponent,
+    NewComponent,
+    HomeComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
